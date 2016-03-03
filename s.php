9bbdf90c36a824a0ef1cf064a95c111cb949f4a0
Phildo <phildo211@gmail.com>
2016-03-03 16:42:01 +0000

    functional tests passed!

--- s.php
+++ s.php
@@ -1,6 +1,6 @@
 <?php
   $filename = "data.txt";
-  $event = preg_replace('/[^A-Za-z0-9\-.,]/', '', $_GET["event"]);
+  $event = preg_replace('/[^A-Za-z0-9\-.,\'":]/', '', $_GET["event"]);
 
   $fp = fopen($filename, "r+");
   if(flock($fp, LOCK_EX))
@@ -9,11 +9,13 @@
     if($event)
     {
       $lines = explode("\n",$content);
-      $last_line = $lines[count($lines)-1];
+      $last_line = $lines[count($lines)-2];
       $last_num = intval(substr($last_line,0,strpos($last_line," ")));
       if(!is_numeric($last_num)) $last_num = 0;
-      $content = substr($content,strpos($content,"\n")).$last_num." ".$event."\n";
+      $last_num++;
+      $content = substr($content,strpos($content,"\n")+1).$last_num." ".$event."\n";
       ftruncate($fp, 0);
+      rewind($fp);
       fwrite($fp, $content);
       fflush($fp);
     }
